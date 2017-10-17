bd9666de29fc5ef327cbf0bcaee9a95d9cd5dd10
Kirsten Hazler <kirsten.hazler@dcr.virginia.gov>
2017-10-17 19:09:42 -0400

    Created PrepRoadsVA.py script, based on ModelBuilder model in VulnMod toolbox

---
+++ PrepRoadsVA.py
@@ -0,0 +1,101 @@
+# ---------------------------------------------------------------------------
+# PrepRoadsVA.py
+# Version:  ArcGIS 10.3.1 / Python 2.7.8
+# Creation Date: 2017-10-17 (adapted from a ModelBuilder model)
+# Last Edit: 2017-10-17
+# Original ModelBuilder Creators:  Kirsten R. Hazler and Tracy Tien
+# Script Creator: Kirsten R. Hazler
+
+# Summary:
+
+# ---------------------------------------------------------------------------
+
+# Import arcpy module
+import arcpy
+
+def PrepRoadsVA(inRCL):
+   """Prepares a Virginia Road Centerlines (RCL) feature class for further processing. This function assumes that there already exist some specific fields, including:
+ - LOCAL_SPEED_MPH 
+ - MTFCC
+ - SEGMENT_EXISTS
+ - RCL_ID
+ If any of the assumed fields do not exist, have been renamed, or are in the wrong format, the script will fail."""
+   # Process: Create "SPEED_cmnt" field.
+   # This field can be used to store QC comments related to speed, if needed.
+   arcpy.AddMessage("Adding 'SPEED_cmt' field...")
+   arcpy.AddField_management (inRCL, "SPEED_cmnt", "TEXT", "", "", 50)
+   
+   # Process: Create and calculate "FlgFld"
+   # This field is used to flag records with suspect LOCAL_SPEED_MPH values
+   # 1 = Flagged: need to update speed based on MTFCC field
+   # -1 = Flagged: need to set speed to 3 (walking pace)
+   # 0 = Unflagged: record assumed to be fine as is
+   arcpy.AddMessage("Adding and populating 'FlgFld' field...")
+   arcpy.AddField_management(inRCL, "FlgFld", "LONG")
+   codeblock = """def Flg (exists, speed):
+      if exists == "Y":
+         if (speed == 0):
+            return 1
+         elif (speed % 5 != 0):
+            return 1
+         else:
+            return 0
+      else:
+         return -1"""
+   expression = "Flg(!SEGMENT_EXISTS!,!LOCAL_SPEED_MPH!)"
+   arcpy.CalculateField_management(inRCL, "FlgFld", expression, "PYTHON_9.3", codeblock)
+
+   # Process: Create and calculate "SPEED_upd" field.
+   # This field is used to store speed values to be used in later processing. It allows for updating according to QC criteria, so that the existing, original "LOCAL_SPEED_MPH" field can remain unaltered. Initially, the "SPEED_upd" field is set to equal the existing "LOCAL_SPEED_MPH" field. It will be updated later if needed.
+   arcpy.AddMessage("Adding and populating 'SPEED_upd' field...")
+   arcpy.AddField_management(inRCL, "SPEED_upd", "LONG")
+   codeblock = """def SpdUpd(flgfld, mtfcc, speed):
+      if flgfld == 1:
+         if mtfcc == 'S1100':
+            return 55
+         elif mtfcc in ['S1200', 'S1300', 'S1640']:
+            return 45
+         elif mtfcc == 'S1630':
+            return 30
+         elif mtfcc in ['C3061', 'C3062', 'S1400', 'S1740']:
+            return 25
+         elif mtfcc in ['S1500', 'S1730', 'S1780']:
+            return 15
+         elif mtfcc == 'S1820':
+            return 10
+         else:  
+            return 3
+      elif flgfld == -1:
+         return 3
+      else:
+         return speed"""
+   expression = "SpdUpd(!FlgFld!, !MTFCC!, !LOCAL_SPEED_MPH!)"
+   arcpy.CalculateField_management(inRCL, "SPEED_upd", expression, codeblock)
+
+   # Process: Create and calculate "TravTime" field
+   # This field is used to store the travel time, in minutes, required to travel 1 meter, based on the road speed designation.
+   arcpy.AddMessage("Adding and populating 'TravTime' field...")
+   arcpy.AddField_management(inRCL, "TravTime", "DOUBLE")
+   expression = "0.037/ !SPEED_upd!"
+   arcpy.CalculateField_management(inRCL, "TravTime", expression, "PYTHON_9.3")
+
+   # Process: Create and calculate the "RmpHwy" field
+   # This field indicates if a road is a limited access highway (2), a ramp (1), or any other road type (0)
+   arcpy.AddMessage("Adding and populating 'RmpHwy' field...")
+   arcpy.AddField_management(inRCL, "RmpHwy", "SHORT")
+   codeblock = """def RmpHwy (mtfcc):
+      if mtfcc in ("S1100", "S1100HOV"):
+         return 2
+      if mtfcc = "S1630":
+         return 1
+      else:
+         return 0"""
+   expression = "RmpHwy(!MTFCC!)"
+   arcpy.CalculateField_management(inRCL, "RmpHwy", expression, "PYTHON_9.3", codeblock)
+
+   # Process: Create and calculate the "UniqueID"
+   # This field stores a unique ID with a state prefix for ease of merging data from different states.
+   arcpy.AddMessage("Adding and populating 'UniqueID' field...")
+   arcpy.AddField_management(inRCL, "UniqueID", "TEXT", "", "", "16")
+   expression = "'VA_' +  str(int(!RCL_ID!))"
+   arcpy.CalculateField_management(inRCL, "UniqueID", expression, "PYTHON_9.3")
