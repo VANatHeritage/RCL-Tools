f8d5e919a834c9cebc6a9d5205d122f1f4d245c3
Kirsten Hazler <kirsten.hazler@dcr.virginia.gov>
2017-10-24 09:55:44 -0400

    Updated documentation for ProcRoads.py

--- ProcRoads.py
+++ ProcRoads.py
@@ -3,18 +3,20 @@
 # Version:  ArcGIS 10.3.1 / Python 2.7.8
 # Creator: Kirsten R. Hazler
 # Creation Date: 2017-10-17 
-# Last Edit: 2017-10-23
+# Last Edit: 2017-10-24
 
 # Summary:
-# A collection of functions for processing roads data and using them for various analyses.
+# A collection of functions for processing roads data to prepare them as inputs for various analyses.
 
 # Usage tips:
-# Use the following function sequence to do a travel time analysis based on a cost surface (raster):
-# - PrepRoadsVA
-# - PrepRoadsTIGER
-# - MergeRoads
-# - 
-
+# Use the following function sequence to prepare roads for travel time analysis:
+# - PrepRoadsVA (to prepare Virginia RCL data)
+# - PrepRoadsTIGER (to prepare TIGER roads data from adjacent states)
+# - MergeRoads (to merge the RCL and TIGER datasets into a seamless dataset, with a limited set of critical fields in the output)
+# 
+# The following functions are helper functions called by the above functions:
+# - printMsg (shortcut for informative progress messaging)
+# - ProjectToMatch (to project one dataset to match the coordinate system of another)
 # ---------------------------------------------------------------------------
 
 # Import modules
